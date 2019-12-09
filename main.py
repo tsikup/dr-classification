a8159e69dd6610d66bc3b4c0297f780e1a1ef85f
Nikos Tsiknakis <tsiknakisn@gmail.com>
2019-12-09 14:56:24 +0200

    corrected import for tf.keras module

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# from data_loader.simple_mnist_data_loader import SimpleMnistDataLoader
-# from models.simple_mnist_model import SimpleMnistModel
-# from trainers.simple_mnist_trainer import SimpleMnistModelTrainer
-# from utils.config import process_config
-# from utils.dirs import create_dirs
-# from utils.utils import get_args
+from data_loader.simple_mnist_data_loader import SimpleMnistDataLoader
+from models.simple_mnist_model import SimpleMnistModel
+from trainers.simple_mnist_trainer import SimpleMnistModelTrainer
+from utils.config import process_config
+from utils.dirs import create_dirs
+from utils.utils import get_args
 
 def main():
     # capture the config path from the run arguments
@@ -16,19 +16,19 @@ def main():
         exit(0)
 
     # create the experiments dirs
-    # create_dirs([config.callbacks.tensorboard_log_dir, config.callbacks.checkpoint_dir])
+    create_dirs([config.callbacks.tensorboard_log_dir, config.callbacks.checkpoint_dir])
 
     print('Create the data generator.')
-    # data_loader = SimpleMnistDataLoader(config)
+    data_loader = SimpleMnistDataLoader(config)
 
     print('Create the model.')
-    # model = SimpleMnistModel(config)
+    model = SimpleMnistModel(config)
 
     print('Create the trainer')
-    # trainer = SimpleMnistModelTrainer(model.model, data_loader.get_train_data(), config)
+    trainer = SimpleMnistModelTrainer(model.model, data_loader.get_train_data(), config)
 
     print('Start training the model.')
-    # trainer.train()
+    trainer.train()
 
 
 if __name__ == '__main__':
