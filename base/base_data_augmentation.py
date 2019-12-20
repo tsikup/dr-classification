4146180968f2963e33127d0aa1b66f2b81f396fe
Nikos Tsiknakis <tsiknakisn@gmail.com>
2019-12-20 10:41:04 +0200

    deleted augmentation files

--- base/base_data_augmentation.py
+++
@@ -1,9 +0,0 @@
-class BaseAugmentation(object):
-    def __init__(self,data):
-        self.data = data
-
-    def augment(self):
-        #implement data augmentation
-        raise NotImplementedError
-
-     #other supporting methods
--- data_augmentation/kaggle_eyepacs_augmentation.py
+++
@@ -1,9 +0,0 @@
-from base.base_data_augmentation import BaseAugmentation
-
-class SimpleMnistPreprocess(BaseAugmentation):
-    def __init__(self,data):
-        super(BaseAugmentation, self).__init__(data)
-
-    def augment(self):
-        #implement data preprocessing
-        pass
