e5c553d337de8945f46ca39eac4d799891dbaa05
Nikos Tsiknakis <tsiknakisn@gmail.com>
2020-02-05 17:18:50 +0200

    added Inception V3 model

--- models/dr_classification_models.py
+++ models/dr_classification_models.py
@@ -1,23 +1,28 @@
 from base.base_model import BaseModel
 from tensorflow.keras import Model
-from tensorflow.keras.applications.resnet import ResNet50
-from tensorflow.keras.layers import Input, Dense, GlobalAveragePooling2D, Dropout
+from tensorflow.keras.applications import *
+from tensorflow.keras.layers import Input, Dense, GlobalAveragePooling2D, Dropout, Conv2D
+
+import time
 
 class DR_ResNet50(BaseModel):
     def __init__(self, config):
         super(DR_ResNet50, self).__init__(config)
+        self.input_shape = tuple(self.config.model.input_shape)
         self.build_model()
 
     def build_model(self):
         # Define input tensor
-        self.visible = Input(shape=(512,512,3))
+        self.visible = Input(shape=self.input_shape)
 
         # ResNet50 as backbone network
         # Load pre-trained ResNet50 without the classifier
-        self.resnet50_base = ResNet50(include_top=False, input_tensor=self.visible, weights='imagenet')
-        # Freeze ResNet50 parameters
-        self.resnet50_base.trainable = False
-
+        self.resnet50_base = ResNet50(include_top=False, input_tensor=self.visible, input_shape=self.input_shape, weights='imagenet')
+        # (Un)Freeze ResNet50 parameters
+        for layer in self.resnet50_base.layers:
+            layer.trainable = True
+        # self.resnet50_base.trainable = True
+        
         ### Add custom classifier
         ## W. Zhang, J. Zhong, S. Yang, Z. Gao, J. Hu, Y. Chen and Z. Yi, 
         ## "Automated identification and grading system of diabetic retinopathy using deep neural networks," 
@@ -47,17 +52,52 @@ class DR_ResNet50(BaseModel):
               optimizer = self.config.model.optimizer,
               metrics = ['accuracy'])
 
+class DR_InceptionV3(BaseModel):
+    def __init__(self, config):
+        super(DR_InceptionV3, self).__init__(config)
+        self.input_shape = tuple(self.config.model.input_shape)
+        self.build_model()
 
+    def build_model(self):
+        # Define input tensor
+        self.visible = Input(shape=self.input_shape)
 
+        # InceptionV3 as backbone network
+        # Load pre-trained InceptionV3 without the classifier
+        self.inceptionv3_base = InceptionV3(include_top=False, input_tensor=self.visible, input_shape=self.input_shape, weights='imagenet')
+        # (Un)Freeze InceptionV3 parameters
+        for layer in self.inceptionv3_base.layers:
+            layer.trainable = True
+        # self.inceptionv3_base.trainable = True
+        
+        ### Add custom classifier
+        ## W. Zhang, J. Zhong, S. Yang, Z. Gao, J. Hu, Y. Chen and Z. Yi, 
+        ## "Automated identification and grading system of diabetic retinopathy using deep neural networks," 
+        ## Knowledge-Based Systems, vol. 175, pp. 12-25, 1 7 2019.
+        # GAP
+        self.average_pooling = GlobalAveragePooling2D()(self.inceptionv3_base.output)
+        # Block 1
+        self.hidden_1 = Dense(1024, activation='relu')(self.average_pooling)
+        self.dropout_1 = Dropout(0.25)(self.hidden_1)
+        # Block 2
+        self.hidden_2 = Dense(512, activation='relu')(self.dropout_1)
+        self.dropout_2 = Dropout(0.5)(self.hidden_2)
+        # Block 3
+        self.hidden_3 = Dense(256, activation='relu')(self.dropout_2)
+        self.dropout_3 = Dropout(0.5)(self.hidden_3)
+        # Block 4
+        self.hidden_4 = Dense(128, activation='relu')(self.dropout_3)
+        self.dropout_4 = Dropout(0.5)(self.hidden_4)
 
+        self.output = Dense(5, activation='softmax')(self.dropout_4)
 
+        # Define model
+        self.model = Model(inputs=self.visible, outputs=self.output)
 
-
-
-
-
-
-
+        self.model.compile(
+              loss = self.config.model.loss,
+              optimizer = self.config.model.optimizer,
+              metrics = ['accuracy'])
 
 
         # conv_1      =   Conv2D(32, kernel_size=(7,7), strides = 2, padding = 'valid', activation='relu')(visible_0)
