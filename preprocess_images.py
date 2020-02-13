17dbbf0548677bdb9611392ab39e1c09edf42a5a
Nikos Tsiknakis <tsiknakisn@gmail.com>
2020-02-13 15:48:40 +0200

    added option for choosing number of blur channels to be computed for each image

--- preprocess_images.py
+++ preprocess_images.py
@@ -9,13 +9,13 @@ from concurrent.futures import ProcessPoolExecutor
 from multiprocessing import cpu_count
 import math
 
-def preprocess_and_write(dest, image_path, class_num):
+def preprocess_and_write(dest, image_path, class_num, numOfBlurred=3):
     # Create KagglePreprocess instance
     kaggle_preprocessor = KagglePreprocess()
     # read image in color mode
     image = cv.imread(image_path, 1)
     # preprocess image
-    new_image = kaggle_preprocessor.preprocess(image, numOfBlurred=3)
+    new_image = kaggle_preprocessor.preprocess(image, numOfBlurred=numOfBlurred)
     # determite new filepath
     new_image_filepath = os.path.join(dest, str(class_num), os.path.basename(image_path))
     # save new image
@@ -25,15 +25,16 @@ def chunked_worker(input_tuple):
     dest = input_tuple[0]
     img_file_list = input_tuple[1]
     class_num = input_tuple[2]
-    [preprocess_and_write(dest, img, class_num) for img in img_file_list]
+    numOfBlurred = input_tuple[3]
+    [preprocess_and_write(dest, img, class_num, numOfBlurred) for img in img_file_list]
     return 0
 
-def call_multi_executor(images, write_dir, class_num, max_workers=4):
+def call_multi_executor(images, write_dir, class_num, max_workers=4, numOfBlurred=3):
     with ProcessPoolExecutor(max_workers=max_workers) as executor:
         chunksize = int(math.ceil(len(images) / float(max_workers)))
         for i in range(max_workers):
             images_chuck = images[(chunksize * i) : (chunksize * (i + 1))]
-            executor_input_tuple = (write_dir, images_chuck, class_num)
+            executor_input_tuple = (write_dir, images_chuck, class_num, numOfBlurred)
             executor.submit(chunked_worker, executor_input_tuple)
 
 def main():
@@ -56,6 +57,12 @@ def main():
     # Directory to read testing set
     test_dir = config.dataset.test
     
+    # How many blurred images
+    numOfBlurred = config.preprocessing.numOfBlurred
+    
+    # Max multiprocessing workers
+    workers = config.multiprocess.workers
+    
     # Directory to write training set
     train_dest = config.dataset.train_dest
     Path(train_dest).mkdir(parents=True, exist_ok=False)
@@ -76,7 +83,7 @@ def main():
         # Determine training images' paths
         train_images = glob.glob(os.path.join(train_dir, str(class_num), '*.jpeg'))
         # Preprocess images on several processors
-        call_multi_executor(train_images, train_dest, class_num, max_workers=4)
+        call_multi_executor(train_images, train_dest, class_num, max_workers=workers, numOfBlurred=numOfBlurred)
             
         # ###################### #
         # Preprocess testing set #
@@ -86,7 +93,7 @@ def main():
         # Determine testing images' paths
         test_images = glob.glob(os.path.join(test_dir, str(class_num), '*.jpeg'))
         # Iterate through the images
-        call_multi_executor(test_images, test_dest, class_num, max_workers=4)
+        call_multi_executor(test_images, test_dest, class_num, max_workers=workers, numOfBlurred=numOfBlurred)
         
 
 if __name__ == '__main__':
