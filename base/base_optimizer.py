12bc8348d6db7fddf2cc582ffe9867dde023835d
Nikos Tsiknakis <tsiknakisn@gmail.com>
2020-02-19 17:03:04 +0200

    added self.config attribute

--- base/base_optimizer.py
+++ base/base_optimizer.py
@@ -1,5 +1,6 @@
 class BaseOptimizer(object):
     def __init__(self, config):
+        self.config = config
         self.optimizer_name = self.config.model.optimizer
         self.init_learning_rate = self.config.model.learning_rate
         self.optimizers = {
