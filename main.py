47717d75c9562a4c6b571811ea6f48d8be150829
Nikos Tsiknakis <tsiknakisn@gmail.com>
2020-02-24 13:44:16 +0200

    splitted main function to train or eval mode

--- main.py
+++ main.py
@@ -10,24 +10,7 @@ from utils.gpus import set_gpus
 import tensorflow as tf
 import os
 
-def main():
-    # capture the config path from the run arguments
-    # then process the json configuration file
-    try:
-        args = get_args()
-        config = process_config(args.config)
-    except:
-        print("missing or invalid arguments")
-        exit(0)
-
-    # Set number of gpu instances to be used
-    # set_gpus(config)
-    os.environ["CUDA_VISIBLE_DEVICES"] = config.devices.gpu.id
-    
-    print('Physical devices: {}'.format(len(tf.config.experimental.list_physical_devices('GPU'))))
-    print('Logical devices: {}'.format(len(tf.config.experimental.list_logical_devices('GPU'))))
-    
-    # create the experiments dirs
+def train(config):
     print('Creating directories: {}, {}, {}'.format(config.callbacks.tensorboard_log_dir, config.callbacks.checkpoint_dir, config.results.performance_dir))
     create_dirs([config.callbacks.tensorboard_log_dir, config.callbacks.checkpoint_dir, config.results.performance_dir])
 
@@ -51,6 +34,48 @@ def main():
     print('Test the model.')
     tester.test()
     
+def evaluate(config):
+    print('Creating directories: {}'.format(config.results.performance_dir))
+    create_dirs([config.results.performance_dir])
+    
+    print('Create the data generator. Classes 0,1 and 2,3,4 are respectively merged together.')
+    data_loader = DRDataLoader(config)
+    test_data = data_loader.get_test_data(classes=config.dataset.classes)
+
+    print('Create the model.')
+    model = DR_InceptionV3(config)
+    
+    print('Loading checkpoint\'s weights')
+    model.load(config.tester.checkpoint_path)
+    
+    print('Create the tester.')
+    tester = DRModelTester(model.model, test_data, config)
+    
+    print('Test the model.')
+    tester.test()
+
+def main():
+    # capture the config path from the run arguments
+    # then process the json configuration file
+    try:
+        args = get_args()
+        config = process_config(args.config)
+    except Exception as e:
+        print(e)
+        print("missing or invalid arguments")
+        exit(0)
+
+    # Set number of gpu instances to be used
+    # set_gpus(config)
+    os.environ["CUDA_VISIBLE_DEVICES"] = config.devices.gpu.id
+    
+    print('Physical devices: {}'.format(len(tf.config.experimental.list_physical_devices('GPU'))))
+    print('Logical devices: {}'.format(len(tf.config.experimental.list_logical_devices('GPU'))))
+    
+    if(config.mode == "train"):
+        train(config)
+    elif(config.mode == "eval"):
+        evaluate(config)
 
 if __name__ == '__main__':
     main()
