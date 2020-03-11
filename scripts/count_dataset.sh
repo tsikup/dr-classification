a142f4d79bb98e5b5bf55a0a5df4678d67211d24
Nikos Tsiknakis <tsiknakisn@gmail.com>
2020-03-11 17:02:20 +0200

    script to count dataset

--- scripts/count_dataset.sh
+++ scripts/count_dataset.sh
@@ -1,4 +1,4 @@
-data_dir=${1:-"/media/hdd/tsiknakisn/SeeFar/Kaggle/EyePACS/all_data_preprocessed"}
+data_dir=${1:-"/media/hdd/tsiknakisn/SeeFar/Kaggle/EyePACS/all_data"}
 
 tr0="$(ls $data_dir/train/0 | wc -l)" || tr0=-1
 tr1="$(ls $data_dir/train/1 | wc -l)" || tr1=-1
@@ -13,7 +13,11 @@ ts2="$(ls $data_dir/test/2 | wc -l)" || ts2=-1
 ts3="$(ls $data_dir/test/3 | wc -l)" || ts3=-1
 ts4="$(ls $data_dir/test/4 | wc -l)" || ts4=-1
 
+echo "--------------------------"
+
 echo Data directory: $data_dir
+
+echo "--------------------------"
 
 echo Train/0: $tr0 
 echo Train/1: $tr1 
@@ -21,10 +25,22 @@ echo Train/2: $tr2
 echo Train/3: $tr3 
 echo Train/4: $tr4
 
+echo "--------------------------"
+
 echo Test/0: $ts0 
 echo Test/1: $ts1 
 echo Test/2: $ts2 
 echo Test/3: $ts3 
 echo Test/4: $ts4 
+
+echo "--------------------------"
+
+echo Training Sum: $(($tr0 + $tr1 + $tr2 + $tr3 + $tr4))
+
+echo "--------------------------"
+
+echo Testing Sum: $(($ts0 + $ts1 + $ts2 + $ts3 + $ts4))
+
+echo "--------------------------"
 
 echo Sum: $(($tr0 + $tr1 + $tr2 + $tr3 + $tr4 + $ts0 + $ts1 + $ts2 + $ts3 + $ts4))
